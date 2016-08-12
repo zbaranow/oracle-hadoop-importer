f33993fc0c96896df036802ca12a8275b862287e
Zbigniew Baranowski <zbigniew.baranowski@cern.ch>
2016-08-12 18:42:41 +0200

    OraDecoder refactorized

--- src/main/java/org/kite/DBImporter/OraParquetImport.java
+++ src/main/java/org/kite/DBImporter/OraParquetImport.java
@@ -513,9 +513,7 @@ public class OraParquetImport{
 
      }
 
-
-
-      
+     
 
       public void start ()
       {
@@ -532,6 +530,7 @@ public class OraParquetImport{
 
    }
 }
+
    class DataWriter{
 
 	private DatasetDescriptor dsd;
@@ -603,7 +602,7 @@ public class OraParquetImport{
 		for (int i=0; i<RowSchema.size(); i++)
 		{
 			try{
-				builder.set(RowSchema.get(i+1).get("name"),castColType(cols[i],RowSchema.get(i+1).get("type")));
+				builder.set(RowSchema.get(i+1).get("name"),OraDecoder.castColType(cols[i],RowSchema.get(i+1).get("type")));
 			}
 			catch(ArrayIndexOutOfBoundsException e)
 			{
@@ -621,173 +620,186 @@ public class OraParquetImport{
 		
 	}
 
-        private Object castColType(byte[] data,String type)
-	{
-		Object odata=null;
-		switch(type)
-		{
-			case "CHAR":
-				odata = castVarchar(data);
-				break;
-			case "NUMBER":
-				NUMBER n = new NUMBER(data);
-				odata=n.doubleValue();
-				break;
-			case "TIMESTAMP":
-				odata = castTimestamp(data);
-				break;
-			case "LHCLOG.VECTORNUMERIC":
-
-
-				odata = castArray(data,"NUMRIC");
-				break;
-
-		}
-		return odata;
-		
-	}
-	 private List<Object> castArray(byte[] data,String type)
-	 {
-		OraArray arr = new OraArray(data,type);
-		return arr.elements;
-	 }
-
-	private Object castVarchar(byte[] data){
-		return (Object)new String(data,StandardCharsets.UTF_8);
-	}
-        public static Object castNumber(byte[] data){
-		
-		boolean positive = true;
-		int exp=0;
-		int value=0;
-		String number=null;
-              
-
-		for (int i=0;i<data.length;i++)
-		{
-			value=getInt(data[i]);
-//			System.out.println(value);
-			if(i==0)
-			{
-			   if (value==128) return 0;
-			   if (value>127)
-			   {
-				exp=value-192;
-				number="";
-					
-			   }
-			   else 
-			   {
-//				System.out.println();
-				positive=false;
-				exp=63-value;
-				number="-";
-
-			   }
-			   if (exp<0)
-			   {
-				number=String.format("%."+(-exp*2)+"f",0.0);
-			   }
-			   continue;
-			}
-			if(exp==i-1) number+=".";
-			if(positive)
-			{
-				number+=String.format("%02d",value-1);
-			}
-			else{
-				if(i!=data.length-1)
-					number+=String.format("%02d",101-value);
-				
-			}
-						
-			
-		}
-		if(number!=null&&exp>(data.length-1))
-		     for(int j=data.length-1;j<exp;j++) number+="00";
-		return (Object) Double.valueOf(number);
-	}
-	public static int getInt(byte b)
-	{
-	        return b & 0xFF;
-		
-	}
-        private Object castTimestamp(byte[] data){
-                String date="";
-		double nano;
-		long unixtime=0;
-		int value=0;
-		byte[] nanoS= new byte[4];
-                for (int i=1;i<data.length;i++){
-			value=getInt(data[i]);
-			switch(i){
-				case 1: date+= String.format("%02d", value - 100 );
-					break;
-				case 2: date+= String.format("%02d",value);
-					break;
-				case 3: date+= String.format("%02d",value);
-					break;
-				case 4: date+= String.format("%02d",value - 1);
-					break;
-				case 5: date+= String.format("%02d",value - 1);
-					break;
-				case 6: date+= String.format("%02d",value - 1);
-					break;
-				default:
-					nanoS[i-7]=data[i];	
-					break;
-			}
-		}
-		nano=(new BigInteger(nanoS).intValue())
-			/(double)1000000;
-
-		DateFormat dfm = new SimpleDateFormat("yyMMddHHmmss"); 
-		dfm.setTimeZone(TimeZone.getTimeZone("GMT"));
-		try
-    		{
-			//timestamp in ms
-		        unixtime = dfm.parse(date).getTime();  
-
-			
-		} 
-		catch (ParseException e) 
-		{
-		        e.printStackTrace();
-		}		
-//		System.out.println(date+"="+(unixtime+nano));
-                return (Object) (unixtime+nano);
-
-        }
-
         public void close(){
 		writer.close();
 	}
    }
 
-   class OraArray
-   {
-	private byte[] raw;
-	public List<Object> elements;
-	private int arraySize=0;
-	public int length=0;
-	public String type = null;
 
-	OraArray(byte[] data,String type)
-	{
-		raw=data;
-		parse(raw,type);
+   class OraDecoder
+   {
+	 public static Object castColType(byte[] data,String type)
+        {
+                Object odata=null;
+                switch(type)
+                {
+                        case "CHAR":
+                                odata = OraSimpleDecoder.castVarchar(data);
+                                break;
+                        case "NUMBER":
+                                odata= OraSimpleDecoder.castNumber(data);
+                                break;
+                        case "TIMESTAMP":
+                                odata = OraSimpleDecoder.castTimestamp(data);
+                                break;
+                        case "LHCLOG.VECTORNUMERIC":
+
+
+                                odata = OraArrayDecoder.castArray(data,"NUMRIC");
+                                break;
+
+                }
+                return odata;
+
+        }
+
+
+   }
+
+   class OraSimpleDecoder
+   {
+
+	
+	 public static Object castTimestamp(byte[] data){
+                String date="";
+                double nano;
+                long unixtime=0;
+                int value=0;
+                byte[] nanoS= new byte[4];
+                for (int i=1;i<data.length;i++){
+                        value=getInt(data[i]);
+                        switch(i){
+                                case 1: date+= String.format("%02d", value - 100 );
+                                        break;
+                                case 2: date+= String.format("%02d",value);
+                                        break;
+                                case 3: date+= String.format("%02d",value);
+                                        break;
+                                case 4: date+= String.format("%02d",value - 1);
+                                        break;
+                                case 5: date+= String.format("%02d",value - 1);
+                                        break;
+                                case 6: date+= String.format("%02d",value - 1);
+                                        break;
+                                default:
+                                        nanoS[i-7]=data[i];
+                                        break;
+                        }
+                }
+                nano=(new BigInteger(nanoS).intValue())
+                        /(double)1000000;
+
+                DateFormat dfm = new SimpleDateFormat("yyMMddHHmmss");
+                dfm.setTimeZone(TimeZone.getTimeZone("GMT"));
+                try
+                {
+                        //timestamp in ms
+                        unixtime = dfm.parse(date).getTime();
+
+
+                }
+                catch (ParseException e)
+                {
+                        e.printStackTrace();
+                }
+//              System.out.println(date+"="+(unixtime+nano));
+                return (Object) (unixtime+nano);
+
 	}
-	public  void dumpRawArray(byte[] data)
+
+	public static Object castVarchar(byte[] data){
+                return (Object)new String(data,StandardCharsets.UTF_8);
+        }
+
+	public static Object castNumber(byte[] data)
+	{
+		NUMBER n = new NUMBER(data);
+		return n.doubleValue();
+	}
+		
+        public static Object castNumber2(byte[] data){
+
+                boolean positive = true;
+                int exp=0;
+                int value=0;
+                String number=null;
+
+
+                for (int i=0;i<data.length;i++)
+                {
+                        value=getInt(data[i]);
+//                      System.out.println(value);
+                        if(i==0)
+                        {
+                           if (value==128) return 0;
+                           if (value>127)
+                           {
+                                exp=value-192;
+                                number="";
+
+                           }
+                           else
+                           {
+//                              System.out.println();
+                                positive=false;
+                                exp=63-value;
+                                number="-";
+
+                           }
+                           if (exp<0)
+                           {
+                                number=String.format("%."+(-exp*2)+"f",0.0);
+                           }
+                           continue;
+                        }
+                        if(exp==i-1) number+=".";
+                        if(positive)
+                        {
+                                number+=String.format("%02d",value-1);
+                        }
+                        else{
+                                if(i!=data.length-1)
+                                        number+=String.format("%02d",101-value);
+
+                        }
+
+
+                }
+                if(number!=null&&exp>(data.length-1))
+                     for(int j=data.length-1;j<exp;j++) number+="00";
+                return (Object) Double.valueOf(number);
+        }
+        public static int getInt(byte b)
+        {
+                return b & 0xFF;
+
+        }
+
+
+}
+   
+   class OraArrayDecoder
+   {
+	public static void dumpRawArray(byte[] data)
        	{
                	for(int i = 0; i < data.length ; i ++ ){
                        	System.out.println(i+"/"+data.length+": "+getInt(data[i]));
 		}              	
         }
+	public static List<Object> castArray(byte[] data,String type)
+        {
+               return getList(data,type);
+        }
 
-	private void parse(byte[] data,String type)
+
+	public static List<Object> getList(byte[] data,String type)
 	{
 		int state = 0;
 		int v=0;
+		List<Object> elements;
+		int arraySize=0;
+		int length=0;
+
 		elements = new ArrayList<Object>();
 		for(int i=2;i<data.length;i++)
 		{
@@ -824,7 +836,7 @@ public class OraParquetImport{
                                                break;
 
 				case 3: //getting elements value
-					elements.add(DataWriter.castNumber(Arrays.copyOfRange(data, i+1, i+v+1)));
+					elements.add(OraSimpleDecoder.castNumber(Arrays.copyOfRange(data, i+1, i+v+1)));
 					i+=v;
 					break;
 			}
@@ -837,15 +849,15 @@ public class OraParquetImport{
 			 v=data[-1];
 		}
 	
-		
+	  return elements;		
       }
-	 private int getInt(byte b)
+	 private static int getInt(byte b)
         {
                 return b & 0xFF;
 	}
 
 	
-   	    
-   }
+   }   	    
+   
 
 
